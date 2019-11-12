802d031af884f726606ddc043763b04929b0c089
lauren.sciullo <lauren.sciullo@MU-C1-06.mullen.pvt.k12.co.us>
2019-11-12 11:35:45 -0700

    start of project, created classes

---
+++ CreditCardNumberValidater/src/inputNumbers.java
@@ -0,0 +1,15 @@
+
+public class inputNumbers
+	{
+		public static void greeting()
+		{
+			System.out.println("Hello! Welcome to the Credit Card Number Validater!");
+			input();
+		}
+		
+		public static void input()
+		{
+			
+		}
+
+	}
---
+++ CreditCardNumberValidater/src/reportValidation.java
@@ -0,0 +1,11 @@
+
+public class reportValidation
+	{
+
+		public static void main(String[] args)
+			{
+				inputNumbers.greeting();
+
+			}
+
+	}
